08ac39508380bd422c614cc9c60c56d95bee7c15
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-07-30 15:42:25 -0400

    Initial Commit

---
+++ LetitGo/LetitGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetitGoApp.swift
+//  LetitGo
+//
+//  Created by Амир Кайдаров on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LetitGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
